ea118d4c8e0070e26473b284d4ec344566ab10aa
felix3646 <felix.sturm2004@icloud.com>
2024-04-23 14:09:49 +0200

    Aufgabe62

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 from my_functions import build_person
 import json
-from my_classes import Person, Experiment
+from my_classes import Person, Experiment, Subject, Examiner
 
 first_name = input("Geben Sie den Vornamen an:")
 last_name = input("Geben Sie den Nachnamen an:")
 age = int(input("Geben Sie das Alter an:"))
 sex = input("Geben Sie das Geschlecht an(male or female):")
+birthday = input("Geben Sie das Geburstdatum an:")
 
-person1 = Person(first_name, last_name, sex, age) 
+person1 = Subject(first_name, last_name, age, sex, birthday) 
 person1.save()
 
-experiment_name = input("Geben Sie den Experimentennamen an:")
-date = input("Geben Sie das Datum an:")
-supervisor = input("Geben Sie den Versuchsleiter an:")
-subject = input("Geben Sie die Versuchsperson an:")
-
-experiment1 = Experiment(experiment_name, date, supervisor, subject)
-experiment1.save()
--- my_classes.py
+++ my_classes.py
@@ -3,21 +3,57 @@ import json
 
 class Person():
 
-    def __init__(self, first_name, last_name, age, sex):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-        self.age = age
-        self.sex = sex
+        
         self.__dict__ = { "first_name" : self.first_name,
-                        "last_name" : self.last_name,
-                        "age" : self.age,
-                        "estimate_max_hr" : estimate_max_hr(self.age, self.sex)}
-    def estimate_max_hr(self):
-        return estimate_max_hr(self.age, self.sex)
+                        "last_name" : self.last_name}
     
     def save(self):
         with open("person.json", "w") as f: 
             json.dump(self.__dict__, f)
+
+class Subject(Person):
+
+    def __init__(self, first_name, last_name, age, sex, birthday):
+        super().__init__(first_name, last_name)
+
+        self.age = age
+        self.sex = sex
+        self.__birthday = birthday
+        self.max_heart_rate = self.estimate_max_hr()
+        self.__dict__ = { "first_name" : self.first_name,
+                        "last_name" : self.last_name,
+                        "age" : self.age,
+                        "sex" : self.sex,
+                        "max_heart_rate" : self.max_heart_rate}
+        
+    def estimate_max_hr(self):
+         return estimate_max_hr(self.age, self.sex)
+         
+    
+    
+    def save(self):
+        with open("subject.json", "w") as f: 
+            json.dump(self.__dict__, f)
+        
+class Examiner(Person):
+
+    def __init__(self, first_name, last_name, id_number):
+        super().__init__(first_name, last_name)
+
+
+        self.id_number = id_number
+        self.__dict__ = { "first_name" : self.first_name,
+                        "last_name" : self.last_name,
+                        "id_number" : self.id_number}
+        
+        
+    def save(self):
+        with open("examiner.json", "w") as f: 
+            json.dump(self.__dict__, f)
+        
 
 class Experiment():
 
@@ -36,5 +72,6 @@ class Experiment():
             json.dump(self.__dict__, f)
 
 if __name__ == "__main__":
-    person = Person("felix", "sturm", 24, "male")
+    person = Subject("felix", "sturm", 34, "male", "21.3.2003")
+    print(person.max_heart_rate)
     person.save()
